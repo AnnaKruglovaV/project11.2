ccabd847fcdcf1ffb7c126accd6346e821fb1808
Anna Kruglova <anya.kruglova.16@gmail.com>
2023-12-25 21:37:01 +0400

    change str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,7 @@
 def to_upper(input_string:str):
     '''функция, которая делает заглавные буквы'''
     return input_string.upper()
+
+def capital_letters(input_string:str):
+    '''функция, которая выводит первые буквы заглавными'''
+    return input_string.title()
